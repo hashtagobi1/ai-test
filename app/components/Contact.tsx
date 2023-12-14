9c4128ad602a72cb28779580a55f0cfe525b44c4
hashtagobi1 <anokwuruobi@gmail.com>
2023-12-14 22:59:39 +0000

    added contact, made text prettier

---
+++ app/components/Contact.tsx
@@ -0,0 +1,22 @@
+const Contact = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <a href="mailto:anokwuruobi@gmail.com?subject=Front%20End%20Developer%20interview&body=Hi%20Obi%2C%20we%20have%20seen%20your%20AI%20demo%20and%20we%20would%20love%20to%20discuss%20next%20steps.">
+        <button className="btn  hover:text-white text-transparent bg-gradient-to-r from-pink-500 via purple-500 via-white to-indigo-500 bg-clip-text rounded-full mr-5">
+          Contact Obi
+        </button>
+      </a>
+      <a
+        href="https://obi-portfolio.vercel.app/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <button className="btn hover:text-white text-transparent bg-gradient-to-r from-pink-500 via purple-500 via-white to-indigo-500 bg-clip-text rounded-full rounded-full btn-outline ">
+          View Portfolio
+        </button>
+      </a>
+    </div>
+  );
+};
+
+export default Contact;
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,13 +1,13 @@
 // import type { MetaFunction } from "@remix-run/node";
-import { ActionFunctionArgs, LoaderFunctionArgs, json } from "@remix-run/node";
+import { ActionFunctionArgs, json } from "@remix-run/node";
 import {
   Form,
-  ShouldRevalidateFunctionArgs,
   useActionData,
   useLoaderData,
   useNavigation,
 } from "@remix-run/react";
 import { useEffect, useState } from "react";
+import Contact from "../components/Contact";
 import { createPrompt } from "../utils/prodia";
 // export const meta: MetaFunction = () => {
 //   return [
@@ -16,9 +16,8 @@ import { createPrompt } from "../utils/prodia";
 //   ];
 // };
 
-export const loader = async ({ request }: LoaderFunctionArgs) => {
+export const loader = async () => {
   return json(await createPrompt("puppies in a cloud, 4k"));
-  return json([]);
 };
 
 export async function action({ request }: ActionFunctionArgs) {
@@ -50,54 +49,59 @@ export default function Index() {
     setInputValue(e.target.value);
   };
   return (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content text-center">
-        <div className="max-w-md flex flex-col  justify-center items-center">
-          <h1 className="text-5xl font-bold">AI Playground 🤖</h1>
-          <p className="py-6">
-            Creations beyond your wildest dreams... Enter a prompt below.
-          </p>
+    <>
+      <Contact />
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content text-center">
+          <div className="max-w-md flex flex-col  justify-center items-center">
+            <h1 className="text-5xl font-bold text-transparent bg-gradient-to-r from-pink-500 via purple-500 to-indigo-500 bg-clip-text p-2">
+              AI Playground 🤖
+            </h1>
+            <p className="py-6">
+              Creations beyond your wildest dreams... Enter a prompt below.
+            </p>
 
-          <Form encType="multipart/form-data" method="post">
-            <label className="form-control w-full max-w-xs">
-              <input
-                value={inputValue}
-                type="text"
-                placeholder={inputValue}
-                onChange={handleChange}
-                name="prompt"
-                className="input input-bordered w-full max-w-xs mb-3"
-              />
-              <div className="label">
-                <span className="label-text-alt opacity-40">
-                  Hint: &quot;a man in space planting a tree&quot;
-                </span>
-              </div>
-              <button type="submit" className="btn btn-primary">
-                Create
-              </button>
-            </label>
-          </Form>
+            <Form encType="multipart/form-data" method="post">
+              <label className="form-control w-full max-w-xs">
+                <input
+                  value={inputValue}
+                  type="text"
+                  placeholder={inputValue}
+                  onChange={handleChange}
+                  name="prompt"
+                  className="input input-bordered w-full max-w-xs mb-3"
+                />
+                <div className="label">
+                  <span className="label-text-alt opacity-40">
+                    Hint: &quot;a man in space planting a tree&quot;
+                  </span>
+                </div>
+                <button type="submit" className="btn btn-primary">
+                  Create Image
+                </button>
+              </label>
+            </Form>
 
-          <div className="m-2 mt-20 p-4 border rounded-md">
-            {navigation.state === "submitting" ||
-            navigation.state === "loading" ? (
-              <div className="">
-                <h1 className="mb-3 font-lg italic">Submitting Request</h1>
-                <span className="loading loading-spinner loading-lg"></span>
-              </div>
-            ) : (
-              <div>
-                <h3 className="font-bold text-4xl">Your Image</h3>
-                <p className="text-slate-400 text-lg my-4">
-                  Last Prompt: {serverPrompt}
-                </p>
-                <img src={serverImage} alt={`prompt: ${prompt}`} />
-              </div>
-            )}
+            <div className="m-2 mt-20 p-4 border rounded-md">
+              {navigation.state === "submitting" ||
+              navigation.state === "loading" ? (
+                <div className="">
+                  <h1 className="mb-3 font-lg italic">Submitting Request</h1>
+                  <span className="loading loading-spinner loading-lg"></span>
+                </div>
+              ) : (
+                <div>
+                  <h3 className="font-bold text-4xl">Your Image</h3>
+                  <p className="text-slate-400 text-lg my-4">
+                    Last Prompt: {serverPrompt}
+                  </p>
+                  <img src={serverImage} alt={`prompt: ${prompt}`} />
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
